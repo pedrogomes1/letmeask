21e57913e401b652034a57a116d65d87046df379
Pedro <pedro-gomes17@hotmail.com>
2021-06-26 23:29:01 -0300

    chore: added use of authentication hook in application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app';
+import { AuthContextProvider } from '../contexts/AuthContext';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
 export default MyApp;
